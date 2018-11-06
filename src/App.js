4df9d3ca369bd7588a4d0fed880e80705ac2c5b2
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-06 15:46:15 -0500

    criteria met

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+// in node js, it would be require/module.exports (if someone else requires package, the stuff inside would be all the methods and when person imports, it would be the const package = req("ekr") then package.methdo() and yadayada); the pairs here are iport/export 
+import React, { Component } from 'react'; // base class from react library; this is importing certain parts of the module; runtime that react will run from this file
+import logo from './giffyGif.gif';
 import './App.css';
+import Text from "./Text.jsx";
 
-class Text extends Component {
-  render() {
-    return (
-      <h1>Hello, <Text style="color: orange" value="Test" /></h1>
-    )
-  }
-}
-
+// with class syntax expand from component class, like grid class; class App is var name that can be used anywhere 
 class App extends Component {
   render() {
     return (
@@ -20,10 +15,18 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+          
+          {/* values on attributes become props inside the component/render, and the props get used for things like styling */}
+          <h1>Hello, <Text value="Michelle" color="orange" border="20px dotted magenta"/></h1>
+          
+          {/* these are their own obj */}
+          <Text value="eosfnb" color="pink">testing</Text>
         </header>
       </div>
     );
   }
 }
 
+// import and export are pairs; ES6 module syntax
+// export defaultl when importing, do the import Module from './filename' in some other file
 export default App;
---
+++ src/Text.jsx
@@ -0,0 +1,23 @@
+// import/export is new feature in js (new and importevd), not just react; part of regular js syntax dont see often because browsers dont support syntax much yet; create-react-app will convert points from import statements so this code can run in the broswer
+import React, { Component } from 'react';
+
+class Text extends Component {
+    // // dont really need this here b/c not doing anything different than what deaulf is doing, which looks exactyly like this; if doing more than this, have this and w/e else
+    // constructor(props) {
+    //     // super calls constructor of parent/component class; in prototype, would component.callthis(); in class, do super and dont need call and this and parent; the options are called props here 
+    //     super(props);
+    // }
+    // instruction set of how to look when renders
+    // lifecycle method (declarative; react controls lifecycle and we write instructions on how behaves)
+    // section that text component creates; could be used in any other component like it's own html tag; create 1 comp to uuse in another by composing 
+    render() {
+        return (
+            // need js obj w/ scc rulse ; innerset is for js obj,. and second set is for where put js code; when inside jsx delinate wher put js like ${ inside `; js is inside the curlies }
+            // this is from the paticular instance of text comp and is used in the other file; once constructor runs, it makes new obj that is comp instance and react handles that object but htis is how it sould look; react makes instance
+            <span style={{ color: this.props.color, border: this.props.border }}>{this.props.value}!</span>
+                // span keeps it on the same line when it's inside another part of code, but anything else puts it on another 
+        )
+    }
+}
+
+export default Text; // --> to help import
