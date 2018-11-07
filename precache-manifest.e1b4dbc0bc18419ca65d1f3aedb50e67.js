76f37a3493463100378295f97e24880c28d9c8d3
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-07 10:06:14 -0500

    Updates

---
+++ precache-manifest.e1b4dbc0bc18419ca65d1f3aedb50e67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc92b504364545c5698b",
+    "url": "/hello-react/static/css/main.98b60ce0.chunk.css"
+  },
+  {
+    "revision": "bc92b504364545c5698b",
+    "url": "/hello-react/static/js/main.bc92b504.chunk.js"
+  },
+  {
+    "revision": "d61ade21e6036ae2c8d8",
+    "url": "/hello-react/static/js/1.d61ade21.chunk.js"
+  },
+  {
+    "revision": "2ac5259eac6c393b99aa",
+    "url": "/hello-react/static/js/runtime~main.2ac5259e.js"
+  },
+  {
+    "revision": "d5963c6f0bc206e3723f796e3b54fd6b",
+    "url": "/hello-react/static/media/giffyGif.d5963c6f.gif"
+  },
+  {
+    "revision": "6ce78ee7c99831399383364f85e9fc1a",
+    "url": "/hello-react/index.html"
+  }
+];
